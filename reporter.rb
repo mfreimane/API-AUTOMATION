b1b543fd0be492ca6c3828d6bf69fbb16991638e
Madara Freimane <madarafreimane@Madaras-MacBook-Pro.local>
2018-06-20 18:30:03 +0300

    message2

--- reporter.rb
+++ reporter.rb
@@ -1,12 +1,18 @@
 require_relative 'features/support/api_helper'
 require 'json'
 
+job_name = ARGV[0]
+job_number = ARGV[1]
+
 thumbnail = { 'url' => 'https://pbs.twimg.com/profile_images/662215297378574336/YOOD0ijr_400x400.png'}
 
 fields = []
 
 fields.push({'name' => 'Autors', 'value' => 'Madara'})
 fields.push({'name' => 'Merķis', 'value' => 'kurss'})
+fields.push({'name' => 'Job', 'value' => job_name})
+fields.push({'name' => 'Build number', 'value' => job_number})
+
 embed = []
 embed.push('title' => 'Test title',
            'colour' => 16007746,
